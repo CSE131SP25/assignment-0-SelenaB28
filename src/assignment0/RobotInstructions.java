5fecef54e3af39ded3f1ee32858ca977ee62db9e
selena <selena@localhost>
2025-01-21 22:18:03 -0600

    three-leaf clover drawing

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,67 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(150);
+		robot.turnRight(150);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnRight(150);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnRight(150);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnLeft(30);
+		robot.forward(50);
 	}
 
 }
